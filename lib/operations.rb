cc01505e6607ba2e4f0eef66576ea23fadcddde6
Kristopher Bullinger <kris.bullinger@gmx.com>
2018-07-09 14:32:51 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def unsafe?(speed)
 	if (speed > 60 || speed < 40)
 		return true
@@ -11,3 +12,16 @@ end
 def not_safe?(speed)
 	return (speed < 40 || speed > 60) ? true : false
 end
+=======
+def unsafe?(speed)
+	if (speed > 60 || speed < 40)
+		return true
+	else
+		return false
+	end
+end
+
+def not_safe?(speed)
+	return (speed < 40 || speed > 60) ? true : false
+end
+>>>>>>> 5396ea777a9115dab6ef3c257a3da9e767b0d216
